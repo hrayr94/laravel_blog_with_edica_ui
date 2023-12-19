84b938751b3b1c66578ad0e3d2ed4c15c726a16a
Hrayr Ashchyan <hrayrashchyan94@gmail.com>
2023-12-20 02:09:19 +0400

    start crud

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::group(['App\Http\Controllers\namespace' => 'Main'], function () {
 
 Route::prefix('admin')->group(function () {
     Route::get('/', [\App\Http\Controllers\Admin\Main\IndexController::class, "__invoke"]);
+    Route::get('/categories', [\App\Http\Controllers\Admin\Category\IndexController::class, "__invoke"])->name('admin.category.index');
+    Route::get('/categories/create', [\App\Http\Controllers\Admin\Category\CreateController::class, "__invoke"])->name('admin.category.create');
 });
 
 
